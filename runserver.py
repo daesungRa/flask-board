ed6efebc1c9ca9cb077defe68165d346f684a914
daesungRa <daesungRa@gmail.com>
2019-06-10 18:40:30 +0900

    modify timeline script

--- runserver.py
+++ runserver.py
@@ -1,7 +1,7 @@
 from src import app
 from src.model import connection_count
 from flask import session
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 from datetime import datetime
 
 socketio = SocketIO(app)
@@ -11,29 +11,32 @@ conn_count = connection_count.Connection_count()
 def connect():
     if 'nickname' in session:
         conn_count.add_conn(1)
-        emit('response', {'stat': 'connect', 'data': 'New Member', 'nickname': session['nickname'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
+        response_data = {'stat': 'connect', 'data': 'New Member', 'nickname': session['nickname'], 'tot': conn_count.get_tot(), 'time': datetime.now().strftime('%H:%M:%S')}
     else:
         conn_count.add_conn(2)
-        session['guest_num'] = str(conn_count.get_guest())
-        emit('response', {'stat': 'connect', 'data': 'New Guest', 'nickname': 'guest' + session['guest_num'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
+        session['nickname'] = 'guest' + str(conn_count.get_guest())
+        response_data = {'stat': 'connect', 'data': 'New Guest', 'nickname': session['nickname'], 'tot': conn_count.get_tot(), 'time': datetime.now().strftime('%H:%M:%S')}
 
-@socketio.on("request", namespace="/timeline")
-def request(timeline):
-    print("timeline : " + timeline['data'])
-    if 'nickname' in session:
-        emit('response', {'stat': 'request', 'data': timeline['data'], 'nickname': session['nickname'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
-    else:
-        emit('response', {'stat': 'request', 'data': timeline['data'], 'nickname': 'guest' + session['guest_num'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
+    emit('response', response_data, broadcast=True)
+
+@socketio.on("timeline", namespace="/timeline")
+def timeline(timeline):
+    data = timeline['data']
+    nickname = timeline['nickname']
+    print("[nickname]timeline : " + "[" + nickname + "]" + data)
+    print(session['nickname'])
+
+    # 타임라인 요청에 담긴 송신자의 nickname 과 현재 접속 세션의 nickname 을 비교하는 것이지만,
+    # 같은 호스트 내에서 전송된 요청이라면 서로 다른 탭일지라도 구분이 불가하므로,
+    # 일단은 테스트를 위해 다음과 같이 작성한다.
+    # 추후 배포를 한다면, 비교 로직을 추가할 것 (nickname != session['nickname'])
+    emit('response', {'stat': 'timeline', 'data': data, 'nickname': nickname, 'tot': conn_count.get_tot(), 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
 
 @socketio.on("disconnect", namespace="/timeline")
-def disconnect():
+def disconnect(msg):
     print('disconnected')
-    if 'nickname' in session:
-        conn_count.sub_conn()
-        emit('response', {'stat': 'disconnect', 'data': 'Disconnected', 'nickname': session['nickname'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
-    else:
-        conn_count.sub_conn()
-        emit('response', {'stat': 'disconnect', 'data': 'Disconnected', 'nickname': 'guest' + session['guest_num'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
+
+    emit('disconnect', {'data': 'Disconnected', 'nickname': session['nickname'], 'time': datetime.now().strftime('%H:%M')}, broadcast=True)
     session.clear()
 
 if __name__ == '__main__':
--- src/static/js/socketio.js
+++ src/static/js/socketio.js
@@ -2,25 +2,26 @@ function socketio() {
     var sock = io.connect('http://localhost:5000/timeline');
     var nickname = '';
 
+    // receive message
     sock.on('response', function(msg) {
         if ($('.timeline-container .timeline-content').text() == 'No Contents') {
             $('.timeline-container .timeline-content').html('<ul><li>Socket_io Connected</li></ul>');
-        }
+        };
 
         if (msg.stat == 'connect') {
-            nickname = msg.nickname;
+            nickname = msg['nickname'];
             $('.timeline-container .timeline-content ul').append('<li>' + ' [Hello] ' + msg.data + ' (' + msg.nickname + ', ' + msg.time + ')' + '</li>');
+            $('.timeline-container .timeline-title .tot-connection').text('현재 접속자 수: ' + msg.tot)
             console.log('Received Hello Message');
-        } else {
-            if (nickname != msg.nickname) {
-                $('.timeline-container .timeline-content ul').append('<li>' + '   >> [' + msg.nickname + '] ' + msg.data + ' (' + msg.time + ')' + '</li>');
-            }
-            console.log('Received Message : ' + msg.data);
+        } else if (msg.stat == 'timeline') {
+            $('.timeline-container .timeline-content ul').append('<li>' + '   >> [' + msg.nickname + '] ' + msg.data + ' (' + msg.time + ')' + '</li>');
+            $('.timeline-container .timeline-title .tot-connection').text('현재 접속자 수: ' + msg.tot)
+
+            console.log('Received Message');
         }
+    })
 
-    });
-
-    // message send
+    // send message
     $('.timeline-form #my-timeline').keyup(function(e) {
         if (e.keyCode == '13') {
             $('.timeline-form #btn-send').trigger('click');
@@ -35,7 +36,7 @@ function socketio() {
         var now = date.getHours() + ':' + date.getMinutes();
 
         $('.timeline-container .timeline-content ul').append('<li style="text-align: right;">(' + now + ') ' + timeline + '</li>');
-        sock.emit('request', {data: timeline});
+        sock.emit('timeline', {data: timeline, nickname: nickname});
         $('.timeline-form #my-timeline').val('');
     });
 }
